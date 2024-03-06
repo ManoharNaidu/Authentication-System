ad5992e0a1343c3e7bfd192a4c336c7e1c06b623
ManoharNaidu <beesettim27@gmail.com>
2024-03-06 12:05:29 +0530

    Added tokens using JWT

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ require("dotenv").config();
 require('./config/database').connect();
 
 const express = require('express');
-const user = require('./models/user');
+const User = require('./models/user');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 app.use(express.json());
@@ -14,28 +15,41 @@ app.get('/', (req, res) => {
 })
 
 
-app.get("/register", async (req,res) => {
-    const { firstname, lastname, email, password } = req.body;
+app.post("/register", async (req,res) => {
+    try {
+        const { firstname, lastname, email, password } = req.body;
     
-    if (!(firstname && lastname && email && password)) {
-        res.status(400).send("All input is required");
+        if (!(firstname && lastname && email && password)) {
+            res.status(400).send("All input is required");
+        }
+    
+        const existingUser = await User.findOne({ email})
+    
+        if (existingUser) {
+            res.status(409).send("User already exists");
+
+        }
+    
+        const EncryptedPassword = await bcrypt.hash(password, 10);
+    
+        const newUser = await User.create({firstname, lastname, email, password: EncryptedPassword})
+    
+        // token generation
+        const token = jwt.sign(
+            {user_id : newUser._id, email},
+            process.env.SECRET_KEY,
+            { expiresIn : "5h"}
+        )
+        newUser.token = token
+    
+        // password issue
+        newUser.password = undefined;
+
+        // update or not
+        res.status(201).json(newUser);
     }
-
-    const existingUser = await user.findOne({ email})
-
-    if (existingUser) {
-        res.status(409).send("User already exists");
-    }
-
-    EncryptedPassword = await bcrypt.hash(password, 10);
-
-    user
-    .create({firstname, lastname, email, password: EncryptedPassword})
-    .then(console.log('User created'))
-    .catch(error => {
-        console.log('User not conected');
+    catch (error) {
         console.log(error);
-    })
-
+    }
 })
 module.exports = app;
