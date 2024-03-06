520661aee301deaae3ee6e92acbd74f7bd7e9d3f
ManoharNaidu <beesettim27@gmail.com>
2024-03-07 00:49:49 +0530

    Dont know what changes I've made...!

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const { MONGODB_URL } = process.env
 
 exports.connect = () => {
     mongoose
-    .connect(MONGODB_URL,)
+    .connect(MONGODB_URL)
     .then(console.log('DB Connection success'))
     .catch(error => {
         console.log('DB Connection failed');
